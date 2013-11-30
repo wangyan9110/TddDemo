ef5e43bffb476dd7431ff9e572f59db50f90ad86
wangyan9110 <wangyan9110@gmail.com>
2013-11-30 19:38:47 +0800

    app

---
+++ appDemo/js/app/app.js
@@ -0,0 +1,15 @@
+/**
+ * FileName:
+ *
+ * Author:wangyan
+ * Date:2013-11-30 18:56
+ * Version:V1.0.0.0
+ * Email:yywang1991@gmail.com
+ * Describe: 请描述本文件功能
+ *
+ * Change Record:
+ * {        date    name    describe}
+ *
+ */
+
+window.app = {};
---
+++ appDemo/js/uinttest/appTest.js
@@ -0,0 +1,14 @@
+/**
+ * FileName:
+ *
+ * Author:wangyan
+ * Date:2013-11-30 19:38
+ * Version:V1.0.0.0
+ * Email:yywang1991@gmail.com
+ * Describe: 请描述本文件功能
+ *
+ * Change Record:
+ * {        date    name    describe}
+ *
+ */
+
